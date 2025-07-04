b838f155032cf81a79e182e27f9bc02348404bbf
nayosx <nayos144@gmail.com>
2025-07-04 15:17:34 -0600

    test

--- resources/test_resource.py
+++ resources/test_resource.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 
-test_bp = Blueprint("test_bp", __name__, url_prefix="/")
+test_bp = Blueprint("test_bp", __name__, url_prefix="/api/v1/test")
 
 @test_bp.route("/", methods=["GET"])
 def health_check():
