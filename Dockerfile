964d99e441a400ab320878746f34cf3d20c0fe7b
nayosx <nayos144@gmail.com>
2026-01-04 12:05:19 -0600

    Se hacen cambios al entorno de variables

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,13 @@ FROM python:3.11
 
 WORKDIR /app
 
+ENV PYTHONUNBUFFERED=1 \
+    PYTHONDONTWRITEBYTECODE=1
+
 COPY requirements.txt /app/
 
-RUN pip install --no-cache-dir --upgrade pip wheel
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir --upgrade pip wheel \
+ && pip install --no-cache-dir -r requirements.txt
 
 COPY . /app/
 
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import os
 from dotenv import load_dotenv
+load_dotenv(override=False)
+
 from flask import Flask
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from config import Config
 from db import db, init_db
+
 from resources.user_resource import user_bp
 from resources.auth_resource import auth_bp
 from resources.transaction_resource import transaction_bp
@@ -25,17 +27,29 @@ from resources.laundry_processing_step_resource import processing_step_bp
 from resources.laundry_service_log_resource import laundry_service_log_bp
 
 
-load_dotenv()
-
 def create_app():
     app = Flask(__name__)
+
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true" or not app.debug:
+        print("DB_USER:", os.getenv("DB_USER"))
+        print("DB_HOST:", os.getenv("DB_HOST"))
+        print("DB_NAME:", os.getenv("DB_NAME"))
+        print("ENV FILE LOADED")
+
     app.config.from_object(Config)
+
     init_db(app)
     Migrate(app, db)
+
     CORS(
-        app
+        app,
+        resources={r"/*": {"origins": "*"}},
+        allow_headers=["Content-Type", "Authorization"],
+        methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
     )
+
     JWTManager(app)
+
     app.register_blueprint(user_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(payment_type_bp)
@@ -54,15 +68,12 @@ def create_app():
     app.register_blueprint(processing_step_bp)
     app.register_blueprint(laundry_service_log_bp)
 
-    @app.before_first_request
-    def create_tables():
-        db.create_all()
-
     return app
+
 
 application = create_app()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", "5000"))
     debug = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
-    application.run(debug=debug, host="0.0.0.0", port=port)
+    application.run(debug=debug, host="0.0.0.0", port=port, use_reloader=debug)
